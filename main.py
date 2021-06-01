19df95f9e94242ef698bf0ac1fd44fc939121bd5
Phippre <parkercantor29@gmail.com>
2021-06-01 19:14:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+
+import pandas_datareader
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import datetime as dt
+import matplotlib.dates as mdates
 from yahoo_fin.stock_info import *
 import tkinter as tk
 from tkinter import *
@@ -5,32 +14,41 @@ import tkinter.font as font
 from PIL import ImageTk, Image
 import os
 import time
+from mpl_finance import candlestick_ohlc
 
 isMonitoring = False
 
+os.chdir('C:/Users/parke/Documents/GitHub/StonkStuff')
+
+#SETTING UP ROOT WINDOW~~~~~~~~
 root = tk.Tk()
 root.title('~Stonk Stuff v0.1')
-root.geometry('400x400')
+root.geometry('1000x400')
 root.configure()
-root.iconbitmap('StockInfo/rocket2.ico')
+root.iconbitmap('StonkStuff/rocket2.ico')
 root.resizable(False, False)
 root.attributes('-topmost', True)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-space_img = ImageTk.PhotoImage(Image.open("StockInfo/space_2.png"))
+#IMAGES AND FONTS~~~~~~~~~~~~~~
+space_img = ImageTk.PhotoImage(Image.open("StonkStuff/space_2.png"))
 space_label = Label(image = space_img)
 space_label.place(x=-2, y=-2)
 
-moon_img = ImageTk.PhotoImage(Image.open("StockInfo/moon.png"))
+moon_img = ImageTk.PhotoImage(Image.open("StonkStuff/moon.png"))
 moon_label = Label(image=moon_img, borderwidth=0)
 moon_label.place(x=250)
+
+doge_img = ImageTk.PhotoImage(Image.open("StonkStuff/doge3.png"))
+doge_label = Label(image = doge_img, background='#17211E')
+doge_label.place(y = 220)
 
 courier_new = font.Font(family='Courier New', size=20)
 font2 = font.Font(family='Comic Sans MS', size=8)
 font3 = font.Font(family='Comic Sans MS', size=20)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-entry = Entry(root, width=25, borderwidth=1)
-entry.place(x=125, y=5)
-
+#METHODS~~~~~~~~~~~~~~~~~~~~~~~
 def enterStock(event=None):
     global isMonitoring
     global priceLabel
@@ -40,36 +58,57 @@ def enterStock(event=None):
     ticker = entry.get()
     livePrice = get_live_price(ticker.upper())
     priceLabel = Label(root, text=livePrice, bg='#C09F52', foreground='#00FF21', font=courier_new, borderwidth=3, relief="ridge", width=20)
-    priceLabel.place(relx=.10, y=150)
+    priceLabel.place(x=45, y=150)
     tickerLabel = Label(root, text=ticker.upper(), bg='#C09F52', foreground='black', font=courier_new, borderwidth=3, relief='ridge', width=9)
-    tickerLabel.place(relx=.32, y=110)
+    tickerLabel.place(relx=.125, y=110)
     while isMonitoring:
         priceLabel.configure(text=get_live_price(ticker.upper()))
         root.update()
 
-root.bind('<Return>', enterStock)
+
 
 def cancelProcess():
     global isMonitoring
     isMonitoring = False
     tickerLabel.destroy()
     priceLabel.destroy()
-    
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#TEXT ENTRY FIELD~~~~~~~~~~~~~~
+entry = Entry(root, width=25, borderwidth=1)
+entry.place(x=125, y=5)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+#BUTTONS~~~~~~~~~~~~~~~~~~~~~~~
 button = Button(root, text="Much Start", command=enterStock, width=21, borderwidth=1, background='green', relief='ridge', font=font2)
 button.place(x=125, y=35)
 
 cancelButton = Button(root, text="Such Stop", command=cancelProcess, width=21, borderwidth=1, background='maroon', relief='ridge', font=font2)
 cancelButton.place(x=125, y=65)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-doge_img = ImageTk.PhotoImage(Image.open("StockInfo/doge3.png"))
-doge_label = Label(image = doge_img, background='#17211E')
-doge_label.place(y = 220)
+start = dt.datetime(2021, 3, 1)
+end = dt.datetime.now()
+data = pandas_datareader.DataReader('DOGE-USD', 'yahoo', start, end)
+data = data[['Open', 'High', 'Low', 'Close']]
+data.reset_index(inplace=True)
+data['Date'] = data['Date'].map(mdates.date2num)
 
+f = Figure(figsize=(5,5), dpi=100)
+a = f.add_subplot(111)
+a.grid(True)
+a.set_axisbelow(True)
+a.set_title('DOGE Price', color='white')
+a.set_facecolor('black')
+a.figure.set_facecolor('#17211E')
+a.tick_params(axis='x', colors='white')
+a.tick_params(axis='y', colors='white')
+a.xaxis_date()
+candlestick_ohlc(a, data.values, width=.25, colorup='#00ff00')
+canvas = FigureCanvasTkAgg(f, root)
+canvas.draw()
+canvas.get_tk_widget().place(x=400, y=0, width=600, height=400)
+
+
+root.bind('<Return>', enterStock)
 root.mainloop()
-
-
-#ticker = input("Please enter a ticker symbol \n")
-
-#while True:
-    #print(get_live_price(ticker.upper()))
