bb7229932ca0ed810cfae8739f68b7b811d0539a
Phippre <parkercantor29@gmail.com>
2021-06-02 19:44:09 -0400

    v0.2

--- main.py
+++ main.py
@@ -36,16 +36,24 @@ root.attributes('-topmost', True)
 
 #IMAGES AND FONTS~~~~~~~~~~~~~~
 space_img = ImageTk.PhotoImage(Image.open("StonkStuff/res/space_2.png"))
-space_label = Label(image = space_img)
-space_label.place(x=-2, y=-2)
+space_label = Label(image = space_img, borderwidth=0)
+space_label.place(width=400, height=400)
 
 moon_img = ImageTk.PhotoImage(Image.open("StonkStuff/res/moon.png"))
 moon_label = Label(image=moon_img, borderwidth=0)
-moon_label.place(x=250)
+moon_label.place(x=248)
 
 doge_img = ImageTk.PhotoImage(Image.open("StonkStuff/res/doge3.png"))
 doge_label = Label(image = doge_img, background='#17211E')
-doge_label.place(y = 220)
+doge_label.place(x = -6, y = 220)
+
+space_img2 = ImageTk.PhotoImage(Image.open("StonkStuff/res/space_3.png"))
+space_label2 = Label(image = space_img, borderwidth=0)
+space_label2.place(x=395)
+
+space_img3 = ImageTk.PhotoImage(Image.open("StonkStuff/res/space_2.png"))
+space_label3 = Label(image = space_img, borderwidth=0)
+space_label3.place(x=600)
 
 courier_new = font.Font(family='Courier New', size=20)
 font2 = font.Font(family='Comic Sans MS', size=8)
@@ -58,7 +66,6 @@ def updatePrice():
     global ticker
 
     if isMonitoring == True:
-        print(isMonitoring)
         priceLabel.configure(text=get_live_price(ticker.upper()))
         root.after(1000, updatePrice)
     else:
@@ -77,12 +84,41 @@ def enterStock(event=None):
     priceLabel.place(x=45, y=150)
     tickerLabel = Label(root, text=ticker.upper(), bg='#C09F52', foreground='black', font=courier_new, borderwidth=3, relief='ridge', width=9)
     tickerLabel.place(relx=.125, y=110)
+    processGraph()
     updatePrice()
 
     #Previous code for updating price (Slow and freezes screen)
     #while isMonitoring: 
     #    priceLabel.configure(text=get_live_price(ticker.upper()))
     #    root.update()
+
+def processGraph():
+    #Start and end dates for graph
+    start = dt.datetime(2021, 3, 1)
+    end = dt.datetime.now()
+
+    #Data
+    data = pandas_datareader.DataReader('DOGE-USD', 'yahoo', start, end)
+    data = data[['Open', 'High', 'Low', 'Close']]
+    data.reset_index(inplace=True)
+    data['Date'] = data['Date'].map(mdates.date2num)
+
+    #Graphics
+    f = Figure(figsize=(1, 1), dpi=65)
+    a = f.add_subplot(111)
+    a.grid(True)
+    a.set_axisbelow(True)
+    a.set_title('DOGE Price', color='white')
+    a.set_facecolor('#17211E')
+    a.figure.set_facecolor('#17211E')
+    a.tick_params(axis='x', colors='white')
+    a.tick_params(axis='y', colors='white')
+    myFmt = DateFormatter("%m/%d")
+    a.xaxis.set_major_formatter(myFmt)
+    candlestick_ohlc(a, data.values, width=.25, colorup='#00ff00')
+    canvas = FigureCanvasTkAgg(f, root)
+    canvas.draw()
+    canvas.get_tk_widget().place(x=400, y=0, width=600, height=400)
 
 def cancelProcess():
     global isMonitoring
@@ -105,9 +141,9 @@ cancelButton.place(x=125, y=65)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #GRAPH~~~~~~~~~~~~~~~~~~~~~~~~~
-start = dt.datetime(2021, 3, 1)
+""" start = dt.datetime(2021, 3, 1)
 end = dt.datetime.now()
-data = pandas_datareader.DataReader('DOGE-USD', 'yahoo', start, end)
+data = pandas_datareader.DataReader(ticker, 'yahoo', start, end)
 data = data[['Open', 'High', 'Low', 'Close']]
 data.reset_index(inplace=True)
 data['Date'] = data['Date'].map(mdates.date2num)
@@ -126,7 +162,7 @@ a.xaxis.set_major_formatter(myFmt)
 candlestick_ohlc(a, data.values, width=.25, colorup='#00ff00')
 canvas = FigureCanvasTkAgg(f, root)
 canvas.draw()
-canvas.get_tk_widget().place(x=400, y=0, width=600, height=400)
+canvas.get_tk_widget().place(x=400, y=0, width=600, height=400) """
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
